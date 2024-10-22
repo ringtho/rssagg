b30bb8dbc18fdccdafba4a6003ab3fb7e3f63226
Smith Ringtho <sringtho@gmail.com>
2024-10-22 20:45:38 +0300

    Added: connection to server using go-chi

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"github.com/ringtho/rssagg/initializers"
 )
 
@@ -14,12 +16,33 @@ func init() {
 
 
 func main() {
-	fmt.Println("hello world")
+	
 	portString := os.Getenv("PORT")
-
 	if portString == "" {
 		log.Fatal("PORT is not found in the environment")
 	}
 
-	fmt.Println("Port:", portString)
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+  	}))
+
+	srv := &http.Server{
+		Handler: router,
+		Addr: ":"+portString,
+	}
+	
+	log.Printf("Server starting on port %v", portString)
+	err := srv.ListenAndServe()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }

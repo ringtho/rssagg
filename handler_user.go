a4f61969312a29ff70cf60543ffad538178df999
Smith Ringtho <sringtho@gmail.com>
2024-10-24 09:15:10 +0300

    Added feeds migration and updated get user logic to a middleware

--- handler_user.go
+++ handler_user.go
@@ -7,7 +7,6 @@ import (
 	"time"
 
 	"github.com/google/uuid"
-	"github.com/ringtho/rssagg/internal/auth"
 	"github.com/ringtho/rssagg/internal/database"
 )
 
@@ -39,19 +38,7 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 	respondWithJson(w, 201, databaseUserToUser(user))
 }
 
-func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request) {
-	apiKey, err := auth.GetAPIKey(r.Header)
-
-	if err != nil {
-		respondWithErrors(w, 403, fmt.Sprintf("Auth error: %v", err))
-		return
-	}
-	user, err := apiCfg.DB.GetUserByAPIKey(r.Context(), apiKey)
-	if err != nil {
-		respondWithErrors(w, 400, fmt.Sprintf("Couldn't fetch user: %v", err))
-		return
-	}
-
+func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request, user database.User) {
 	respondWithJson(w, 200, databaseUserToUser(user))
 
 }

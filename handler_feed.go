0156a28fd5834df9967f3e90c15a83d31a154e76
Smith Ringtho <sringtho@gmail.com>
2024-10-28 07:43:56 +0300

    Added: Create feed endpoint

---
+++ handler_feed.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/ringtho/rssagg/internal/database"
+)
+
+
+func (apiCfg *apiConfig) handlerCreateFeed(w http.ResponseWriter, r *http.Request, user database.User) {
+	 type parameters struct {
+		Name string `json:"name"`
+		URL string `json:"url"`
+	 }
+
+	 decoder := json.NewDecoder(r.Body)
+	 params := parameters{}
+	 err := decoder.Decode(&params)
+	 if err != nil {
+		respondWithErrors(w, 400, fmt.Sprintf("Error parsing JSON: %v", err))
+		return
+	 }
+
+	 feed, err := apiCfg.DB.CreateFeed(r.Context(), database.CreateFeedParams{
+		ID:       	uuid.New(),
+		CreatedAt: 	time.Now().UTC(),
+		UpdatedAt: 	time.Now().UTC(),
+		Name:      	params.Name,
+		Url:     	params.URL,
+		UserID:   	user.ID,
+	 }) 
+
+	if err != nil {
+		respondWithErrors(w, 400, fmt.Sprintf("Could create a feed: %v", err))
+		return
+	}
+	respondWithJson(w, 201, databaseFeedToFeed(feed))
+}
